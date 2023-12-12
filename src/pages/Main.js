78a5e556afd536ed7d34e7c2fd6d193f51cebe8f
devcodemaestro <Dev.codemaestro@gmail.com>
2023-12-12 15:18:16 +0900

    feat : Drawer 클릭 시 나타날 리스트 배치

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Button, Calendar, Drawer, theme } from "antd";
+import { Avatar, Button, Calendar, Drawer, List, theme } from "antd";
 import "dayjs/locale/ko";
+import { css } from "@emotion/react";
 import dayjs from "dayjs";
 import locale from "antd/es/date-picker/locale/ko_KR";
 import { CloseOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 // 캘린더 영역
 const onPanelChange = (value, mode) => {
   console.log(value);
-  console.log(value.format("YYYY-MM-DD"), mode);
+  // console.log(value.format("YYYY-MM-DD"), mode);
 };
 dayjs.locale("ko");
 
 const Main = () => {
   // Drawer 영역
-  const { token } = theme.useToken();
   const [open, setOpen] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
   const showDrawer = value => {
@@ -26,6 +27,23 @@ const Main = () => {
   const onClose = () => {
     setOpen(false);
   };
+
+  // JSON 테스트용 데이터
+  const jsonData = [
+    {
+      id: 1,
+      title: "일정 1",
+      date: "2023-12-12",
+      image: "photo1.svg",
+    },
+    {
+      id: 2,
+      title: "일정 2",
+      date: "2023-12-12",
+      image: "photo2.svg",
+    },
+    // ... 기타 데이터
+  ];
 
   return (
     <>
@@ -57,7 +75,20 @@ const Main = () => {
         width="70%"
       >
         {selectedDate ? (
-          <p>Selected Date: {selectedDate.format("YYYY-MM-DD")}</p>
+          <List
+            itemLayout="horizontal"
+            dataSource={jsonData} // JSON 데이터를 리스트에 전달
+            renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Avatar src={item.image} />}
+                  title={<Link to={`/details/${item.id}`}>{item.title}</Link>}
+                  description={item.date}
+                />
+                <Button type="text" icon={"더보기"} />
+              </List.Item>
+            )}
+          />
         ) : (
           <p>No date selected</p>
         )}
