c3a7260e77b55f29c18cfd279c55151be182f4c4
kimsomi0126 <kimsomi00126@gmail.com>
2024-03-27 10:44:40 +0900

    feat : Analytics 설정

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import "./styles/normalize.css";
 import "./styles/index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>,
 );
